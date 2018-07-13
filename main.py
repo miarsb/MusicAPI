b9bbe2c19527fe506f81de42cdd3ca80d0cbbebf
branden-bmiars <branden.miars@wpengine.com>
2018-07-12 20:42:21 -0500

    first pass on arg parsing

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 import reddit
 import spotify
+import argparse
 
-subreddit = '/r/listentothis'
-interval = 'day'
-username = 'Branden Miars'
 
-# TODO: python arg parse
+def create_a_playlist(reddit_url, playlist_title, username):
+    reddit_list = reddit.RedditAPI(reddit_url)
+    titles = reddit_list.get_titles()
+
+    spot = spotify.Spotify(titles, playlist_title, username)
+    spot.playlist_creation()
+
 
 if __name__ == '__main__':
-    playlist_title = '{} playlist - {}'.format(subreddit, interval)
-    reddit_list = reddit.RedditAPI('https://www.reddit.com{}/top/.json?t={}'.format(subreddit, interval))
-    titles = reddit_list.get_titles()
-    
-    # TODO spotify should not need reddlit list. limit what spotify knows about reddit Class
-    spotify = spotify.Spotify(titles, playlist_title, username)
-    # creating the list with the spotify track URLs we got in lookUpList()
-    spotify.playlist_creation()
+
+    parser = argparse.ArgumentParser(description='Create Spotify playlists.')
+    parser.add_argument('--reddit', '-r',
+        choices=['day', 'week', 'month'],
+        default='day',
+        help='toggle to pull from reddit. select an interval' )
+    parser.add_argument('--name', '-n',
+        nargs='*',
+        required=True,
+        help='name the playlist')
+    parser.add_argument('--user', '-u',
+        nargs='*',
+        required=True,
+        help='Spotify username')
+    args = parser.parse_args()
+
+    subreddit = '/r/listentothis'
+    interval = args.reddit
+    reddit_url = 'https://www.reddit.com{}/top/.json?t={}'.format(subreddit, interval)
+
+    playlist_title = ' '.join(args.name)
+    username = ' '.join(args.user)
+
+    create_a_playlist(reddit_url, playlist_title, username)
+
+
+
+
+
